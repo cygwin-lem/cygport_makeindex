c4d6ca57bb84b50a6ab11534ce38ec19da960872
Lemures Lemniscati <lemures.lemniscati@gmail.com>
2020-07-12 12:44:38 +0900

    cygport_makeindex.sh

---
+++ cygport_makeindex.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -A ARCH2M
+
+ARCH2M=(
+  ["x86_64"]="x86_64"
+  ["x86"]="i686"
+)
+
+list2html () {
+  echo "<html>"
+  echo "<body>"
+  perl -ne 'chop; print "<li><a href=\"$_\">$_</a></li>\n";'
+  echo "</body>"
+  echo "</html>"
+}
+
+makelist () {
+  for M in "${ARCH2M[@]}"; do
+    find $(find *."${M}" -mindepth 1 -maxdepth 1 -name dist) -type f
+  done
+}
+
+for M in "${ARCH2M[@]}"; do
+  git add $(find *."${M}" -mindepth 1 -maxdepth 1 -name dist -type d)
+done
+
+git rm -f index.html 2> /dev/null
+
+( echo "Packages and hints"; echo ""; git diff --name-only HEAD ) > msg
+
+makelist | list2html > index.html
+git add index.html
+
+git commit -F msg
