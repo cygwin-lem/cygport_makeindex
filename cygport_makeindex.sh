7b34a33870908d3b4fc2948c3ef65061513a3bfd
Lemures Lemniscati <lemures.lemniscati@gmail.com>
2020-10-15 20:50:52 +0900

    Search noarch

--- cygport_makeindex.sh
+++ cygport_makeindex.sh
@@ -5,6 +5,7 @@ declare -A ARCH2M
 ARCH2M=(
   ["x86_64"]="x86_64"
   ["x86"]="i686"
+  ["noarch"]="noarch"
 )
 
 list2html () {
@@ -14,14 +15,26 @@ list2html () {
 }
 
 makelist () {
+  local D
   for M in "${ARCH2M[@]}"; do
-    find $(find *."${M}" -mindepth 1 -maxdepth 1 -name dist) -type f
+    D=$(find *."${M}" -mindepth 1 -maxdepth 1 -name dist -type d 2>/dev/null)
+    if [ -n "$D" ]; then
+      find $D -type f
+    fi
   done
 }
 
-for M in "${ARCH2M[@]}"; do
-  git add $(find *."${M}" -mindepth 1 -maxdepth 1 -name dist -type d)
-done
+gitadd () {
+  local D
+  for M in "${ARCH2M[@]}"; do
+    D=$(find *."${M}" -mindepth 1 -maxdepth 1 -name dist -type d 2>/dev/null)
+    if [ -n "$D" ]; then
+      git add $D
+    fi
+  done
+}
+
+gitadd
 
 git rm -f index.html 2> /dev/null
 
